6c6208cf6d7623cc8305a133b5ba56c3cc06e173
adlermore <armorbita@gmail.com>
2024-04-23 23:27:17 +0400

    mcdonalds is ready

--- mcdonalds/App.js
+++ mcdonalds/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import Categories from './src/components/Categories';
+import Popular from './src/components/Popular';
+import { COLORS } from './constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.CustomContainer}>
+      <Header />
+      <Categories />
+      <Popular />
     </View>
   );
 }
@@ -17,4 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  CustomContainer :{
+    padding : 30,
+    paddingTop : 70,
+    backgroundColor : COLORS.black,
+    height: '100%'
+  }
 });
---
+++ mcdonalds/src/components/CategiryItem.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class CategiryItem extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{color : 'white'}}>CategiryItem</Text>
+      </View>
+    )
+  }
+}
+
+export default CategiryItem
